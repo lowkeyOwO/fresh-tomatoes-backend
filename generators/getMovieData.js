50a7c04115f119da704327902367b2da1541902b
lowkeyOwO <lokesh.varathan1593@gmail.com>
2024-05-24 01:30:15 +0530

    23/05/2024 - Cast & Crew Details

--- generators/getMovieData.js
+++ generators/getMovieData.js
@@ -1,5 +1,16 @@
+function getFilteredCrew(castCrew) {
+  let cast = castCrew["cast"].filter((castMem) => castMem["popularity"] >= 10);
+  let crew = castCrew["crew"].filter((crewMem) => crewMem["job"] === "Director");
+  if (cast.length > 18) {
+    cast = cast.sort((a, b) => b.popularity - a.popularity).slice(0, 18);
+  } 
+  return {cast,crew};
+}
+
+
 export default async function getMovieData(movieId) {
     const movieDataURL = `https://api.themoviedb.org/3/movie/${movieId}`;
+    const movieCreditsURL  = `https://api.themoviedb.org/3/movie/${movieId}/credits`;
     const options = {
       method: "GET",
       headers: {
@@ -9,8 +20,11 @@ export default async function getMovieData(movieId) {
     };
     try {
       const result = await fetch(movieDataURL, options);
+      const movieCred = await fetch(movieCreditsURL,options);
       const response = await result.json();
-      return response;
+      const movieResponse = await movieCred.json();
+      const filteredCrew = getFilteredCrew(movieResponse); 
+      return {...response,...filteredCrew};
     } catch (err) {
       console.error("Error:\t", err);
       return { error: err };
